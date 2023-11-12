8f4c9372ef066cc6c073f0b8f425ba9ccc3ea75e
e-pedidos <epedidosapp2023@gmail.com>
2023-11-12 14:41:29 -0300

    feat: created file theme ts

---
+++ src/style/theme.ts
@@ -0,0 +1,10 @@
+export const themeDefault = {
+    colors:{
+        bluePrimary: '#0F52BA',
+        grayFooter: "#EEEEEE",
+        black: '#000000', 
+        black100: '#373737',
+        black200: '#2C2C2C',
+    },
+    
+}
